d6ffac6d015fdc6144c5d92bbc44f0064b28e3a9
Renato Bezerra <renato-bgs@live.com>
2022-06-24 13:27:55 -0300

    Create api

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './modules/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
+
+
+// GRANT SELECT ON * . * TO 'u488253619_view'@'%';
