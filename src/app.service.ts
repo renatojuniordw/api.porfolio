694d403b1ad60c1c8bbac8d91adbdadbd4b897f7
Renato Bezerra <renato-bgs@live.com>
2022-08-25 18:59:59 -0300

    up

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Auth0!';
+    return 'Hello World Auth0! v1';
   }
 }
