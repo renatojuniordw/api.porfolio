bf19bc52feefec21b8207ad34a22d8a71e0fd55b
Renato Bezerra <renato-bgs@live.com>
2022-08-25 20:49:52 -0300

    up

--- src/config/authz/jwt.strategy.ts
+++ src/config/authz/jwt.strategy.ts
@@ -14,12 +14,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
                 cache: true,
                 rateLimit: true,
                 jwksRequestsPerMinute: 5,
-                jwksUri: `${process.env.AUTH0_ISSUER_URL}.well-known/jwks.json`,
+                jwksUri: `https://${process.env.AUTH0_DOMAIN}.well-known/jwks.json`,
             }),
 
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             audience: process.env.AUTH0_AUDIENCE,
-            issuer: `${process.env.AUTH0_ISSUER_URL}`,
+            issuer: `https://${process.env.AUTH0_DOMAIN}/`,
             algorithms: ['RS256'],
         });
     }
