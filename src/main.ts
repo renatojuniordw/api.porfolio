5809e1969719259daf49acfd462f870f23cb1d43
Renato Bezerra <renato-bgs@live.com>
2022-08-25 19:09:28 -0300

    Update

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(3000 || process.env.PORT);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
